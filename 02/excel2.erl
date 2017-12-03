66eb95a3fc5f37b67d256a117da8a9874ff2bc8b
Mårten Gustafson <marten.gustafson@schibsted.com>
2017-12-03 02:39:50 +0100

    We should rewrite everything in Erlang and perhaps it'd look better if I didn't do it as this hour...

---
+++ 02/excel2.erl
@@ -0,0 +1,43 @@
+-module(excel2).
+-author("marten.gustafson@gmail.com").
+-export([excel2/1]).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+excel2(Spreadsheet) -> excel(0, Spreadsheet).
+
+excel(Checksum, []) -> Checksum;
+excel(Checksum, [Head|Tail]) ->
+  Row = array:from_list(Head),
+  excel(Checksum + row(array:size(Row) - 1, Row, 0), Tail).
+
+row(-1, _, Result) -> Result;
+row(Index, Row, Result) -> 
+  XX = array:sparse_map(fun(Pos, Current) ->
+    Remainder = array:get(Index, Row) / Current,
+    case { floor(Remainder), ceil(Remainder) } of
+      {Whole, Whole} when Pos /= Index -> Whole;
+      {_, _} -> 0
+    end
+  end, Row),
+  row(Index - 1, Row, Result + lists:sum(array:to_list(XX))).
+
+-ifdef(TEST).
+case_one_test() -> 4 = excel2([[5, 9, 2, 8]]).
+case_two_test() -> 9 = excel2([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]).
+-endif.
+
+% It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is,
+% where the result of the division operation is a whole number. They would like you to find those numbers on each line,
+% divide them, and add up each line's result.
+%
+% For example, given the following spreadsheet:
+%
+% 5 9 2 8
+% 9 4 7 3
+% 3 8 6 5
+% In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+% In the second row, the two numbers are 9 and 3; the result is 3.
+% In the third row, the result is 2.
+% In this example, the sum of the results would be 4 + 3 + 2 = 9.
